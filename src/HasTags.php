5f5931d386453edbb3f7b2cfe65d1d4880eb48fd
Freek Van der Herten <freek@spatie.be>
2017-06-12 15:12:00 +0200

    Apply fixes from StyleCI (#49)

--- src/HasTags.php
+++ src/HasTags.php
@@ -2,11 +2,11 @@
 
 namespace Spatie\Tags;
 
+use InvalidArgumentException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use InvalidArgumentException;
 
 trait HasTags
 {
