736e63298c09c2d89b3b610866752fb38d484642
freek <freek@spatie.be>
2017-06-17 01:43:16 +0200

    try with a regular user

--- tests/wait.php
+++ tests/wait.php
@@ -4,7 +4,7 @@ $start = time();
 
 while (true) {
     try {
-        new PDO('mysql:host=127.0.0.1;dbname=laravel-tags', 'root', '');
+        new PDO('mysql:host=127.0.0.1;dbname=laravel-tags', 'user', 'password');
         fwrite(STDOUT, 'Docker container started!' . PHP_EOL);
         exit(0);
     } catch (PDOException $exception) {
