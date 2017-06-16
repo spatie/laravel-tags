80442dca688928623f65ba97e63f8e9791822204
freek <freek@spatie.be>
2017-06-17 01:50:12 +0200

    correct db name

--- tests/wait.php
+++ tests/wait.php
@@ -4,7 +4,7 @@ $start = time();
 
 while (true) {
     try {
-        new PDO('mysql:host=127.0.0.1;dbname=laravel-tags', 'root', '');
+        new PDO('mysql:host=127.0.0.1;dbname=laravel_tags', 'root', '');
         fwrite(STDOUT, 'Docker container started!' . PHP_EOL);
         exit(0);
     } catch (PDOException $exception) {
