9d831e58c6f5d74fa76a715fae2ae269af4dc480
Tanvir Alam <tanvir.alam.shawn@gmail.com>
2018-09-12 13:33:46 +0200

    Stop translating slug

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -6,22 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSlug
 {
-    public static function bootHasSlug()
-    {
-        static::saving(function (Model $model) {
-            collect($model->getTranslatedLocales('name'))
-                ->each(function (string $locale) use ($model) {
-                    $model->setTranslation('slug', $locale, $model->generateSlug($locale));
-                });
-        });
-    }
-
     protected function generateSlug(string $locale): string
     {
         $slugger = config('tags.slugger');
 
         $slugger = $slugger ?: 'str_slug';
 
-        return call_user_func($slugger, $this->getTranslation('name', $locale));
+        return call_user_func($slugger, $this->name);
     }
 }
