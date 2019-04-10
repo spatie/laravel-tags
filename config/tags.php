32e38512193169d5854286fd496cb50ad49c9961
Perry Trinier <perrytrinier@gmail.com>
2019-04-10 09:43:28 -0400

    Fix str_slug being deprecated (#187)

--- config/tags.php
+++ config/tags.php
@@ -1,9 +1,14 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
      * The given function generates a URL friendly "slug" from the tag name property before saving it.
      */
-    'slugger' => 'str_slug',
+    'slugger' => function (string $title) {
+        return Str::slug($title, '-');
+    },
+
 ];
