70463be6240cf20229c6cead31ea9ee63f6b1933
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-29 16:23:11 +0100

    Generate slug from translation

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -11,7 +11,7 @@ trait HasSlug
         static::saving(function (Model $model) {
             collect($model->getTranslatedLocales('name'))
                 ->each(function (string $locale) use ($model) {
-                    $model->setTranslation('slug', $locale, $model->generateSlug($locale));
+                    $model->setTranslation('slug', $locale, $model->generateSlug($model->getTranslation('name', $locale)));
                 });
         });
     }
