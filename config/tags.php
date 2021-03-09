561e771fb31fc7a4234e8e5422f41f77f9a79984
freek <freek@spatie.be>
2021-03-09 19:58:40 +0100

    update doc link

--- config/tags.php
+++ config/tags.php
@@ -4,7 +4,7 @@ return [
 
     /*
      * The given function generates a URL friendly "slug" from the tag name property before saving it.
-     * Defaults to Str::slug (https://laravel.com/docs/5.8/helpers#method-str-slug)
+     * Defaults to Str::slug (https://laravel.com/docs/master/helpers#method-str-slug)
      */
     'slugger' => null,
 
